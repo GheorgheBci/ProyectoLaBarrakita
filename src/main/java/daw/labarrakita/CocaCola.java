72d7ab877eb89b66c3b7b189d5cdd7c07883673a
Gheorghe <georgebucu96@gmail.com>
2020-06-03 12:00:53 +0200

    Añade la clase CocaCola

---
+++ src/main/java/daw/labarrakita/CocaCola.java
@@ -0,0 +1,25 @@
+package daw.labarrakita;
+
+/**
+ *
+ * @author George
+ */
+public class CocaCola extends DecoratorCaja5{
+
+    Caja miCaja;
+
+    public CocaCola(Caja configurable) {
+        this.miCaja = configurable;
+    }
+    
+    @Override
+    public String getDescripcion() {
+         return miCaja.getDescripcion() + "Coca-Cola";
+    }
+
+    @Override
+    public double precio() {
+        return 1.7 + miCaja.precio();
+    }
+    
+}
