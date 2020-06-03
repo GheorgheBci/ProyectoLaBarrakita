1426e8f3d5fce569904dfeec4682d7803a5f90a8
Gheorghe <georgebucu96@gmail.com>
2020-06-03 11:54:38 +0200

    Añade la clase DecoratorCaja5

---
+++ src/main/java/daw/labarrakita/DecoratorCaja5.java
@@ -0,0 +1,14 @@
+package daw.labarrakita;
+
+/**
+ *
+ * @author George
+ */
+public abstract class DecoratorCaja5 extends Caja{
+    
+    // Usaremos la clase Decorator para la caja 5, ya que se podrá configurar a nuestro gusto añadiendo los productos que queramos
+    
+    @Override
+    public  abstract String getDescripcion();
+    
+}
